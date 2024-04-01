beecc25e53e8241e7812f452c90f9532a7390ae5
xquery <best.highloyal@gmail.com>
2024-04-02 01:45:48 +0800

    Add ServerErrorException in __init__.py

--- api/v1/exceptions/__init__.py
+++ api/v1/exceptions/__init__.py
@@ -1,6 +1,8 @@
 from api.v1.exceptions.bad_request_exception import BadRequestException
+from api.v1.exceptions.server_error_exception import ServerErrorException
 
 
 __all__=[
     "BadRequestException",
+    "ServerErrorException"
 ]
