3917f3010892541ba0701cb686209955d624349a
xquery <best.highloyal@gmail.com>
2024-04-02 01:56:27 +0800

    Update create method in sensor_service.py

--- api/v1/services/sensor_service.py
+++ api/v1/services/sensor_service.py
@@ -1,7 +1,19 @@
-from api.v1.schemas import SensorRequestSchema
+from api.v1.schemas import SensorRequestSchema, SensorResponseSchema
+from api.v1.repositories import SensorRepository
+from api.v1.exceptions import BadRequestException, ServerErrorException
+
 
 class SensorService:
     
     @staticmethod
     def create(sensor_id : str, sensor_request : SensorRequestSchema):
-        pass
+        
+        if not SensorRepository.is_sensor_exists:
+            raise BadRequestException(
+                detail="Duplicate instance of sensor exists."
+            )
+        
+        if SensorRepository.create_sensor(sensor_id, sensor_request):
+            return SensorResponseSchema(**sensor_request.dict())
+        
+        raise ServerErrorException()
