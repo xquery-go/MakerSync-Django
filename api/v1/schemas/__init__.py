8fd4b55010221b69a875dac30345f466177e366d
xquery <best.highloyal@gmail.com>
2024-04-01 22:52:43 +0800

    Add ErrorResponseSchema in __init__.py

--- api/v1/schemas/__init__.py
+++ api/v1/schemas/__init__.py
@@ -1,7 +1,9 @@
+from api.v1.schemas.error_schema import ErrorResponseSchema
 from api.v1.schemas.sensor_schema import SensorRequestSchema, SensorResponseSchema
 
 
 __all__=[
+    "ErrorResponseSchema", 
     "SensorRequestSchema",
     "SensorResponseSchema"
 ]
