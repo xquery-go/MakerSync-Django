aeaec5d952136c8c77e59dd3b78787e8c2f67af5
xquery <best.highloyal@gmail.com>
2024-04-01 22:59:36 +0800

    Add create_sensor method in sensor_repository.py

--- api/v1/repositories/sensor_repository.py
+++ api/v1/repositories/sensor_repository.py
@@ -1,3 +1,11 @@
+from firebase_admin import firestore
+
+
+db=firestore.client()
+
+
 class SensorRepository:
     
-    pass
+    @staticmethod
+    def create_sensor(sensor_id : str, sensor_request : SensorRequestSchema):
+        pass
