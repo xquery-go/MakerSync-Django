0330d563bb1822bddc77c3577ab60858ee9b4e67
xquery <best.highloyal@gmail.com>
2024-04-01 23:25:11 +0800

    Add return type in sensor_repository.py

--- api/v1/repositories/sensor_repository.py
+++ api/v1/repositories/sensor_repository.py
@@ -8,10 +8,12 @@ class SensorRepository:
     
     @staticmethod
     def create_sensor(sensor_id : str, sensor_request : SensorRequestSchema):
-        db.collection(sensor_id).document("sensors").set({
+        doc=db.collection(sensor_id).document("sensors")
+        doc.set({
             "is_start" : sensor_request.is_start,
             "is_stop" : sensor_request.is_stop,
             "is_initialized" : sensor_request.is_initialized,
             "counter" : sensor_request.counter,
             "timer" : sensor_request.timer
         })
+        return True
