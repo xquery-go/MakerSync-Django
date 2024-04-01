c198579123b82568eaf732d9bf09d4c2c74ce4aa
xquery <best.highloyal@gmail.com>
2024-04-02 01:57:12 +0800

    Update create method in sensor_controller.py

--- api/v1/controllers/sensor_controller.py
+++ api/v1/controllers/sensor_controller.py
@@ -1,16 +1,31 @@
 from ninja_extra import route, api_controller, ControllerBase
-from api.v1.schemas import SensorRequestSchema, SensorResponseSchema
+from api.v1.schemas import SensorRequestSchema, SensorResponseSchema, ErrorResponseSchema
+from api.v1.services.sensor_service import SensorService
+from api.v1.exceptions import BadRequestException, ServerErrorException
 
 
 @api_controller("/sensor")
 class SensorController(ControllerBase):
     
-    @route.post("/")
-    def create(self, sensor_request : SensorRequestSchema):
+    @route.post("/{sensor_id}", response={
+        201 : SensorResponseSchema, 
+        400 : ErrorResponseSchema,
+        500 : ErrorResponseSchema
+    })
+    def create(self, sensor_id : str, sensor_request : SensorRequestSchema):
         try:
-            pass
-        except Exception as e:
-            print(e)
+            response=SensorResponseSchema(**sensor_request.dict())
+            return 201, response
+        except BadRequestException as e:
+            return 404, ErrorResponseSchema(
+                status=e.status_code, 
+                detail=e.detail
+            )
+        except ServerErrorException as e:
+            return 500, ErrorResponseSchema(
+                status=e.status_code, 
+                detail=e.detail
+            )
     
     @route.get("/{sensor_id}")
     def retrieve(self, sensor_id : str):
