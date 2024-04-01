872e3aa0aa7f3776a05a65be0b7b0aff605a06a9
xquery <best.highloyal@gmail.com>
2024-04-02 01:46:25 +0800

    Add NotFoundException in __init__.py

--- api/v1/exceptions/__init__.py
+++ api/v1/exceptions/__init__.py
@@ -1,8 +1,10 @@
+from api.v1.exceptions.not_found_exception import NotFoundException
 from api.v1.exceptions.bad_request_exception import BadRequestException
 from api.v1.exceptions.server_error_exception import ServerErrorException
 
 
 __all__=[
+    "NotFoundException",
     "BadRequestException",
     "ServerErrorException"
 ]
