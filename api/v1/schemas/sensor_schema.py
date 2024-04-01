92c247e70adf84a6a93515b2123355a58d1eb5e4
xquery <best.highloyal@gmail.com>
2024-04-01 18:37:58 +0800

    Add fields in SensorResquestSchema

--- api/v1/schemas/sensor_schema.py
+++ api/v1/schemas/sensor_schema.py
@@ -1,8 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SensorRequestSchema(BaseModel):
-    pass
+    is_start : bool=Field(
+        Title="Is Start", 
+    )
+    is_stop : bool=Field(
+        Title="Is Stop", 
+    )
+    is_initialized : bool=Field(
+        Title="Is Initialized", 
+    )
+    counter : int=Field(
+        Title="Counter", 
+    )
+    timer : int=Field(
+        Title="Timer", 
+    )
 
 
 
