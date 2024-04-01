18702a44b4bc8007c0a8f56f44920abb273133bf
xquery <best.highloyal@gmail.com>
2024-04-02 01:41:31 +0800

    Remove ready method in apps.py

--- api/v1/apps.py
+++ api/v1/apps.py
@@ -1,16 +1,6 @@
-from pathlib import Path
 from django.apps import AppConfig
-from firebase_admin import credentials, initialize_app
 
 
 class V1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'v1'
-
-
-    def ready(self):
-        current_dir=Path(__file__).resolve().parent.parent
-        service_key=current_dir+"\\serviceAccountKey.json"
-
-        credential=credentials.Certificate(service_key)
-        initialize_app(credetial)
