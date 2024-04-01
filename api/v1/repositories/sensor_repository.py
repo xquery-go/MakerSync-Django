524fe5385d645dba64b0cf460e7ec94adbc1760c
xquery <best.highloyal@gmail.com>
2024-04-01 23:14:08 +0800

    Update create_sensor in sensor_repository.py

--- api/v1/repositories/sensor_repository.py
+++ api/v1/repositories/sensor_repository.py
@@ -8,4 +8,10 @@ class SensorRepository:
     
     @staticmethod
     def create_sensor(sensor_id : str, sensor_request : SensorRequestSchema):
-        pass
+        db.collection(sensor_id).document("sensors").set({
+            "is_start" : sensor_request.is_start,
+            "is_stop" : sensor_request.is_stop,
+            "is_initialized" : sensor_request.is_initialized,
+            "counter" : sensor_request.counter,
+            "timer" : sensor_request.timer
+        })
