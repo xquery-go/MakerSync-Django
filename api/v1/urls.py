45c4e511684e9c9ca370a71990deb5d67e23f2f0
xquery <best.highloyal@gmail.com>
2024-04-01 22:42:45 +0800

    Add SensorController in urls.py

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from ninja import Redoc
 from ninja_extra import NinjaExtraAPI
+from api.v1.controllers import *
 
 
-api = NinjaExtraAPI(docs=Redoc())
-
+api=NinjaExtraAPI()
+api.register_controllers(
+    SensorController
+)
 
 urlpatterns = [
     path("v1/", api.urls)
