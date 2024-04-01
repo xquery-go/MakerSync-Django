b20b8c4a85eb2f318667b0b61ccd96b49a88cbb9
xquery <best.highloyal@gmail.com>
2024-04-01 22:48:34 +0800

    Add request body in sensor_controller.py

--- api/v1/controllers/sensor_controller.py
+++ api/v1/controllers/sensor_controller.py
@@ -1,11 +1,12 @@
 from ninja_extra import route, api_controller, ControllerBase
+from api.v1.schemas import SensorRequestSchema, SensorResponseSchema
 
 
 @api_controller("/sensor")
 class SensorController(ControllerBase):
     
     @route.post("/")
-    def create(self):
+    def create(self, sensor_request : SensorRequestSchema):
         pass
     
     @route.get("/{sensor_id}")
@@ -13,11 +14,13 @@ class SensorController(ControllerBase):
         pass
     
     @route.put("/{sensor_id}")        
-    def update(self, sensor_id : str):
+    def update(self, sensor_id : str, 
+               sensor_request : SensorRequestSchema):
         pass
     
     @route.patch("/{sensor_id}")
-    def partial_update(self, sensor_id : str):
+    def partial_update(self, sensor_id : str, 
+                       sensor_request : SensorRequestSchema):
         pass
     
     @route.delete("/{sensor_id}")
