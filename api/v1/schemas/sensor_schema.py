f5b54e1c370c7515f73cddafd7351fcfd3081d06
xquery <best.highloyal@gmail.com>
2024-04-03 14:42:27 +0800

    Add default values in SensorRequestSchema

--- api/v1/schemas/sensor_schema.py
+++ api/v1/schemas/sensor_schema.py
@@ -4,21 +4,27 @@ from pydantic import BaseModel, Field
 class SensorRequestSchema(BaseModel):
     is_start : bool=Field(
         Title="Is Start",
+        default=False
     )
     is_stop : bool=Field(
         Title="Is Stop", 
+        default=False
     )
     is_initialized : bool=Field(
         Title="Is Initialized", 
+        default=False
     )
     counter : int=Field(
         Title="Counter", 
+        default=0
     )
     timer : int=Field(
         Title="Timer", 
+        default=0
     )
     temperature : float=Field(
-        Title="Temperature"
+        Title="Temperature",
+        default=0
     )
 
 
