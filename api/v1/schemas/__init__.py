443476eb88019e9ed1f8e06e79f7aab8892ae4cd
xquery <best.highloyal@gmail.com>
2024-04-03 16:02:24 +0800

    Add UserRequestSchema and UserResponseSchema import in __init__.py

--- api/v1/schemas/__init__.py
+++ api/v1/schemas/__init__.py
@@ -1,9 +1,13 @@
 from api.v1.schemas.error_schema import ErrorResponseSchema
 from api.v1.schemas.sensor_schema import SensorRequestSchema, SensorResponseSchema
+from api.v1.schemas.user_schema import UserRequestSchema, UserResponseSchema
 
 
 __all__=[
     "ErrorResponseSchema", 
     "SensorRequestSchema",
-    "SensorResponseSchema"
+    "SensorResponseSchema",
+    "UserRequestSchema",
+    "UserResponseSchema"
+    
 ]
