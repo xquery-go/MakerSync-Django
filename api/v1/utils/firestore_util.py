c6b8b5e530ae5e3f365289bd77344d73e0ec992c
xquery <best.highloyal@gmail.com>
2024-04-03 16:07:04 +0800

    Update initialization in firestore_util.py

--- api/v1/utils/firestore_util.py
+++ api/v1/utils/firestore_util.py
@@ -2,13 +2,13 @@ from pathlib import Path
 from firebase_admin import initialize_app, credentials, firestore
 
 
-def firebase_firestore():
-    
-    current_dir=Path(__file__).resolve().parent.parent.parent.parent
-    service_key=f"{current_dir}\\serviceAccountKey.json"
-    
-    credential=credentials.Certificate(service_key)
-    initialize_app(credential)
-    
+current_dir=Path(__file__).resolve().parent.parent.parent.parent
+service_key=f"{current_dir}\\serviceAccountKey.json"
+
+credential=credentials.Certificate(service_key)
+initialize_app(credential)  
+
+
+def firebase_firestore():    
     return firestore.client()
     
