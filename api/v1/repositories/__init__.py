4f6c414960da74fa23d7f06d83e3af36062e2b89
xquery <best.highloyal@gmail.com>
2024-04-03 16:01:03 +0800

    Add UserRepository import in __init__.py

--- api/v1/repositories/__init__.py
+++ api/v1/repositories/__init__.py
@@ -1,6 +1,8 @@
 from api.v1.repositories.sensor_repository import SensorRepository
+from api.v1.repositories.user_repository import UserRepository
 
 
 __all__=[
-    "SensorRepository"
+    "SensorRepository",
+    "UserRepository"
 ]
