b881aa40629883cf62acd774e6c6ed9c6ff3eb5f
xquery <best.highloyal@gmail.com>
2024-04-03 14:48:57 +0800

    Add default values to UserRequestSchema

--- api/v1/schemas/user_schema.py
+++ api/v1/schemas/user_schema.py
@@ -1,9 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserRequestSchema(BaseModel):
-    pass
+    name : str=Field(
+        Title="Name",
+        default=""
+    )
+    email : str=Field(
+        Title="Email",
+        default=""
+    )
+    is_active : bool=Field(
+        Title="Is Active",
+        default=False
+    )
+    is_connected : bool=Field(
+        Title="Is Connected",
+        default=False
+    )
 
 
 class UserResponseSchema(BaseModel):
-    pass
+    name : str
+    email : str
+    is_active : bool
+    is_connected : bool
