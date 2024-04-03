6b762659d33b1ee26447351225f831169a15987c
xquery <best.highloyal@gmail.com>
2024-04-03 15:02:38 +0800

    Add create user method in UserRepository

--- api/v1/repositories/user_repository.py
+++ api/v1/repositories/user_repository.py
@@ -1,2 +1,16 @@
+from api.v1.schemas import UserRequestSchema
+from api.v1.utils import firebase_firestore
+
+
+db=firebase_firestore()
+
+
 class UserRepository:
-    pass
+    
+    @staticmethod
+    def create_user(sensor_id: str, user_request: UserRequestSchema):
+       pass
+   
+   
+    
+    
