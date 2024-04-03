3139dfc05907c7ac2b1a2df593c4f3fea9f7aa5f
xquery <best.highloyal@gmail.com>
2024-04-03 15:27:31 +0800

    Update create method in UserService

--- api/v1/services/user_service.py
+++ api/v1/services/user_service.py
@@ -1,2 +1,37 @@
+from api.v1.schemas import UserRequestSchema, UserResponseSchema
+from api.v1.repositories import UserRepository
+from api.v1.exceptions import BadRequestException, NotFoundException, ServerErrorException
 class UserService:
-    pass
+    
+    @staticmethod    
+    def list(self, sensor_id : str):
+        pass
+    
+    
+    @staticmethod    
+    def create(sensor_id : str, user_request: UserRequestSchema):
+        
+        if not UserRepository.is_user_exists(sensor_id, user_request.email):
+            raise BadRequestException(
+                detail="User already exists."
+            )
+        
+        if not UserRepository.create_user(sensor_id, user_request):
+            raise ServerErrorException()
+        
+        return UserResponseSchema(**user_request.dict())
+    
+    
+    @staticmethod    
+    def retrieve(self, sensor_id : str, user_email : str):
+        pass
+    
+    
+    @staticmethod    
+    def update(self, sensor_id : str, user_email : str):
+        pass
+    
+    
+    @staticmethod    
+    def destroy(self, sensor_id : str, user_email : str):
+        pass
