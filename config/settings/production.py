74d504d7ba13a50ebf857a3b83b2eccad37e56b8
xquery <best.highloyal@gmail.com>
2024-04-04 22:41:56 +0800

    Update allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,5 +3,8 @@ from config.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [
+    ".vercel.app", 
+    ".now.sh",
+    'maker-sync-django.vercel.app']
 
