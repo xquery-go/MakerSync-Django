ad5fbfc750dd65ecc412e8cab811e501d4439bb7
xquery <best.highloyal@gmail.com>
2024-04-04 21:59:20 +0800

    Update run.sh for pipenv

--- run.sh
+++ run.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/bash
 
+
 VENV_PATH="$(pwd)/venv"
+PIPENV_PATH="$(pwd)/Pipfile"
 DEPENDENCIES_PATH="$(pwd)/requirements.txt"
 
-if [ -d "$VENV_PATH" ]; then 
-    echo "Activating virtual environment"
-    source "$VENV_PATH/Scripts/activate"
-else
-    echo "Creating virtual environment"
-    virtualenv "$VENV_PATH"
-    source "$VENV_PATH/Scripts/activate"
 
-    if [ -d "$DEPENDENCIES_PATH" ]; then 
-        python -m pip install -r "$(pwd)/requirements.txt"
+if [ "$1" = "lib" ]; then 
+
+    if [ -d "$VENV_PATH" ]; then 
+        echo "Activating virtual environment"
+        source "$VENV_PATH/Scripts/activate"
+    else
+        echo "Creating virtual environment"
+        virtualenv "$VENV_PATH"
+        source "$VENV_PATH/Scripts/activate"
+    fi
+    
+    if [ -f "$DEPENDENCIES_PATH" ]; then 
+        python -m pip install -r "$DEPENDENCIES_PATH"
     else
         pip install django django-ninja-extra firebase-admin python-dotenv
     fi
     
-    pip install django django-ninja-extra
-fi
+    echo "Running server..."
+    python manage.py runserver
+    deactivate
 
-echo "Running server..."
-python manage.py runserver
-deactivate
+else
 
+    if [ -f "$PIPENV_PATH" ]; then 
+        pipenv shell
+    else
+        pipenv install -r "$DEPENDENCIES_PATH"
+    fi
+
+    echo "Running server..."
+    python manage.py runserver
+    exit
+fi 
