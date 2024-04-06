0ebd85faf1f34aa8878851452b8e2ca22ecafef2
xquery <best.highloyal@gmail.com>
2024-04-06 22:16:56 +0800

    Add documentation in sensor_controller.py

--- api/v1/controllers/sensor_controller.py
+++ api/v1/controllers/sensor_controller.py
@@ -6,15 +6,31 @@ from api.v1.exceptions import BadRequestException, ServerErrorException, NotFoun
 
 @api_controller("/sensors")
 class SensorController(ControllerBase):
-    
-    @route.post("/{sensor_id}", response={
-        201 : SensorResponseSchema, 
-        400 : ErrorResponseSchema,
-        500 : ErrorResponseSchema
-    })
+    """
+    Controller handling operations related to sensors.
+    """
+
+    @route.post("/{sensor_id}", 
+                summary = "Create a new sensor", 
+                description = "Create a new sensor with the provided ID.",
+                response={
+                    201 : SensorResponseSchema, 
+                    400 : ErrorResponseSchema,
+                    500 : ErrorResponseSchema
+                })
     def create(self, sensor_id : str, sensor_request : SensorRequestSchema):
+        """
+        Endpoint to create a new sensor.
+        
+        Args:
+            sensor_id (str): The ID of the sensor.
+            sensor_request (SensorRequestSchema): The request data for creating the sensor.
+        
+        Returns:
+            tuple: A tuple containing status code and response data.
+        """
         try:
-            response=SensorService.create(sensor_id, sensor_request)
+            response = SensorService.create(sensor_id, sensor_request)
             return 201, response
         except BadRequestException as e:
             return 400, ErrorResponseSchema(
@@ -28,15 +44,27 @@ class SensorController(ControllerBase):
             )
             
     
-    @route.get("/{sensor_id}", response={
-        200 : SensorResponseSchema,
-        400 : ErrorResponseSchema,
-        404 : ErrorResponseSchema,
-        500 : ErrorResponseSchema
-    })
+    @route.get("/{sensor_id}",
+               summary = "Retrieve a sensor",
+               description = "Retrieve details of a sensor with the provided ID.", 
+               response = {
+                    200 : SensorResponseSchema,
+                    400 : ErrorResponseSchema,
+                    404 : ErrorResponseSchema,
+                    500 : ErrorResponseSchema
+                })
     def retrieve(self, sensor_id : str):
+        """
+        Endpoint to retrieve details of a sensor.
+        
+        Args:
+            sensor_id (str): The ID of the sensor to retrieve.
+        
+        Returns:
+            tuple: A tuple containing status code and response data.
+        """
         try:
-            response=SensorService.retrieve(sensor_id)
+            response = SensorService.retrieve(sensor_id)
             return 200, response
         except BadRequestException as e:
             return 400, ErrorResponseSchema(
@@ -55,16 +83,29 @@ class SensorController(ControllerBase):
             )
             
     
-    @route.put("/{sensor_id}", response={
-        200 : SensorResponseSchema,
-        400 : ErrorResponseSchema,
-        404 : ErrorResponseSchema,
-        500 : ErrorResponseSchema
-    })        
+    @route.put("/{sensor_id}", 
+               summary = "Update a sensor",
+               description = "Update details of a sensor with the provided ID.",
+               response = {
+                200 : SensorResponseSchema,
+                400 : ErrorResponseSchema,
+                404 : ErrorResponseSchema,
+                500 : ErrorResponseSchema
+                })        
     def update(self, sensor_id : str, 
                sensor_request : SensorRequestSchema):
+        """
+        Endpoint to update details of a sensor.
+        
+        Args:
+            sensor_id (str): The ID of the sensor to update.
+            sensor_request (SensorRequestSchema): The request data for updating the sensor.
+        
+        Returns:
+            tuple: A tuple containing status code and response data.
+        """
         try:
-            response=SensorService.update(sensor_id, sensor_request)
+            response = SensorService.update(sensor_id, sensor_request)
             return 200, response
         except BadRequestException as e:
             return 400, ErrorResponseSchema(
@@ -83,12 +124,24 @@ class SensorController(ControllerBase):
             )
 
     
-    @route.delete("/{sensor_id}", response={
-        204 : dict,
-        404 : ErrorResponseSchema,
-        500 : ErrorResponseSchema
-    })
+    @route.delete("/{sensor_id}", 
+                  summary = "Delete a sensor",
+                  description = "Delete a sensor with the provided ID.",
+                  response={
+                    204 : dict,
+                    404 : ErrorResponseSchema,
+                    500 : ErrorResponseSchema
+                    })
     def destroy(self, sensor_id : str):
+        """
+        Endpoint to delete a sensor.
+        
+        Args:
+            sensor_id (str): The ID of the sensor to delete.
+        
+        Returns:
+            tuple: A tuple containing status code and response data.
+        """
         try:
             if SensorService.destroy(sensor_id):
                 return 204, {"detail" : "Successfully delete collection"}
