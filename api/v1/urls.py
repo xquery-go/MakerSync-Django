5c0a57086283ff6fa4b3793c6e9fd78fd49cafce
xquery <best.highloyal@gmail.com>
2024-04-06 14:47:00 +0800

    Update production environment in urls.py

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,10 +1,24 @@
+import os
+from dotenv import load_dotenv
 from django.urls import path
-from ninja import Redoc
+from ninja import Redoc, Swagger
 from ninja_extra import NinjaExtraAPI
-from api.v1.controllers import *
+from api.v1.controllers import UserController, SensorController
 
 
-api=NinjaExtraAPI(version="1.0.0")
+load_dotenv()
+
+docs = Swagger() 
+environment = os.environ.get("DJANGO_ENV")
+
+if environment.lower() != "development":
+    docs = Redoc()
+    
+api = NinjaExtraAPI(
+    version="1.0.0", 
+    docs=docs
+)
+
 api.register_controllers(
     UserController,
     SensorController
