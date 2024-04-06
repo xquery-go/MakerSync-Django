78832160fa1b54fff7a360560504ed6b9b20e002
xquery <best.highloyal@gmail.com>
2024-04-06 14:15:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,11 +14,12 @@ DEPENDENCIES_PATH="$(pwd)/requirements.txt"
 install_dependencies() {
     # Check if requirements.txt exists
     if [ -f "$DEPENDENCIES_PATH" ]; then 
-        `$1` install -r "$DEPENDENCIES_PATH"
+        $1 install -r "$DEPENDENCIES_PATH"
     else 
-        `$1` install django django-ninja-extra
-        `$1` install firebase-admin python-dotenv
-        `$1` install flake8
+        $1 install python-dotenv
+        $1 install django django-ninja-extra
+        $1 install firebase-admin
+        $1 install flake8
     fi
 }
 
@@ -30,24 +31,25 @@ run_server() {
 
 
 if [ "$1" = "lib" ]; then 
+    echo "Activating virtual environment"
 
     if [ ! -d "$VENV_PATH" ]; then 
         # Creating the python virtual environment 
         virtualenv "$VENV_PATH"
 
+        # Activating python virtual environment
+        source "$VENV_PATH/Scripts/activate"
+
         # Installing the python dependencies
         install_dependencies pip
+    else 
+        # Activating python virtual environment
+        source "$VENV_PATH/Scripts/activate"
     fi
-    
-    echo "Activating virtual environment"
-
-    # Activating python virtual environment
-    source "$VENV_PATH/Scripts/activate"
 
     # Running django local server 
     run_server deactivate
 else
-
     # Initializing pip virtual environment
     pipenv shell
 
