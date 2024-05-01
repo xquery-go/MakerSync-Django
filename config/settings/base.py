3586832df1212ff0fe75475356ce076d8f5729d9
xquery <best.highloyal@gmail.com>
2024-05-01 13:57:59 +0800

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ninja_extra',
-    'api.v1.apps',
+    'api.v1.apps.V1Config',
+    'api.v2.apps.V2Config',
 ]
 
 MIDDLEWARE = [
@@ -50,10 +51,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {}
-
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 LANGUAGE_CODE = 'en-us'
 
@@ -64,4 +82,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-    
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
