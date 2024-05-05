ac4e6b55875ee4f3cd3bd6b6e5ee74825cec484f
xquery <best.highloyal@gmail.com>
2024-05-05 10:19:54 +0800

    Add django admin in urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('api.v1.urls'))
 ]
