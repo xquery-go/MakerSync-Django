b5cca6b05a402ed6feca6b36a396da3619a267b1
xquery <best.highloyal@gmail.com>
2024-05-05 10:44:14 +0800

    Add admin exports in __init__.py

---
+++ api/v2/admins/__init__.py
@@ -0,0 +1,10 @@
+from api.v2.admins.machine_admin import MachineAdmin
+from api.v2.admins.sensor_admin import SensorAdmin
+from api.v2.admins.user_admin import UserAdmin
+
+
+__all__ = [
+    "MachineAdmin",
+    "SensorAdmin",
+    "UserAdmin",
+]
