743fabe060c6d1749d0aac0d02da229f7cd6ac52
xquery <best.highloyal@gmail.com>
2024-05-05 10:44:38 +0800

    Add ready method in apps.py

--- api/v2/apps.py
+++ api/v2/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class V2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.v2'
+
+    
+    def ready(self):
+        
+        from api.v2.admins import (
+            MachineAdmin,
+            SensorAdmin,
+            UserAdmin
+        )
