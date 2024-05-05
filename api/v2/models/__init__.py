56de173d08716b3bed23543b23e808fd165a18ed
xquery <best.highloyal@gmail.com>
2024-05-05 10:31:16 +0800

    Add models exports in __init__.py

---
+++ api/v2/models/__init__.py
@@ -0,0 +1,10 @@
+from api.v2.models.machines import Machine
+from api.v2.models.sensors import Sensor
+from api.v2.models.users import User
+
+
+__all__ = [
+    "Machine",
+    "Sensor",
+    "User"
+]
