c3557ebebce0e72ef4cd7d65a579d3b7025d909c
xquery <best.highloyal@gmail.com>
2024-05-05 22:41:27 +0800

    Add notification admin in __init__.py

--- api/v2/admins/__init__.py
+++ api/v2/admins/__init__.py
@@ -1,10 +1,12 @@
 from api.v2.admins.machine_admin import MachineAdmin
 from api.v2.admins.sensor_admin import SensorAdmin
 from api.v2.admins.user_admin import UserAdmin
+from api.v2.admins.notification_admin import NotificationAdmin
 
 
 __all__ = [
     "MachineAdmin",
     "SensorAdmin",
     "UserAdmin",
+    "NotificationAdmin"
 ]
