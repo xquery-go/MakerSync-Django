0b21060e357cc146617505d410eea1b146d29d93
xquery <best.highloyal@gmail.com>
2024-05-05 14:47:05 +0800

    Update user admin implementation

--- api/v2/admins/user_admin.py
+++ api/v2/admins/user_admin.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from api.v2.models import User
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    
-    def get_fieldsets(self, request, obj = None):
-        
-        fieldsets = super().get_fieldsets(request, obj)
-        print(fieldsets)
-        # if obj:
-        #     fieldsets = (
-        #         (None, {"fields" : ("email", "password")})
-        #     ) + fieldset
-        
-        return fieldsets
-            
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "machine")
