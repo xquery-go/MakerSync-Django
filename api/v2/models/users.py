9cc7d363b3c3f5c04e6c91c93f58259284600fea
xquery <best.highloyal@gmail.com>
2024-05-05 14:45:42 +0800

    Update inheritance in users.py

--- api/v2/models/users.py
+++ api/v2/models/users.py
@@ -1,17 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from api.v2.models import Machine
 
 
-class User(AbstractUser):
-    
-    username = None
-    first_name = None
-    last_name = None
+class User(models.Model):
     
     email = models.EmailField(unique = True, blank = False, null = False)
-    machine = models.ForeignKey("Machine", on_delete = models.CASCADE, blank = False)
-    USERNAME_FIELD = "email"
-    
+    password = models.CharField(max_length = 150)
+    machine = models.ForeignKey(Machine, on_delete = models.CASCADE, blank = False)
+
     
     def __str__(self):
         return self.email
