8b377ef433201dc03f2fc7c8dfa9556edd8f134f
xquery <best.highloyal@gmail.com>
2024-05-05 22:24:48 +0800

    Add notification class in notifications.py

---
+++ api/v2/models/notifications.py
@@ -0,0 +1,13 @@
+from django.db import models
+from api.v2.models import Machine
+
+
+class Notification(models.Model):
+    
+    title = models.CharField(max_length = 100, null = False, blank = False)
+    content = models.TextField(blank = True, null = True)
+    date = models.DateTimeField(auto_now_add = True)
+    machine = models.ForeignKey(Machine, on_delete = models.CASCADE, blank = True)
+    
+    def __str__(self):
+        return self.title 
