3eee809cdbc92eab6253790b4c553cbc96e730d1
xquery <best.highloyal@gmail.com>
2024-05-06 19:25:15 +0800

    Update list_display in user_admin.py

--- api/v2/admins/user_admin.py
+++ api/v2/admins/user_admin.py
@@ -4,4 +4,4 @@ from api.v2.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "email", "machine")
+    list_display = ("id", "username", "email", "machine")
