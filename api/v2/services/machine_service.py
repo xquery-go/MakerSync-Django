515069d5e485924a769f24b79ef0a777c8c9855a
xquery <best.highloyal@gmail.com>
2024-05-06 21:33:25 +0800

    Update create method implementation in machine_service.py

--- api/v2/services/machine_service.py
+++ api/v2/services/machine_service.py
@@ -1,5 +1,18 @@
+from api.v2.schemas import (
+    MachineSchema, SensorSchema)
+from api.v2.exceptions import ( 
+    ConflictException, ServerErrorException)
+
+
 class MachineService:
     
     @staticmethod
-    def create():
-        pass
+    def create(request : MachineSchema):
+        
+        if MachineRepository.is_machine_exist(request):
+            raise ConflictException()
+        
+        if not MachineRepository.create_machine(request):
+            raise ServerErrorException() 
+        
+        return SensorSchema()
