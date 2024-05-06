9edd13bfc509ac48d74fdc22e2ee0efb4a061e20
xquery <best.highloyal@gmail.com>
2024-05-06 22:33:30 +0800

    Update create method in machine_controller.py

--- api/v2/controllers/machine_controller.py
+++ api/v2/controllers/machine_controller.py
@@ -1,10 +1,21 @@
 from ninja_extra import (
     api_controller, route, ControllerBase)
+from api.v2.exceptions import (
+    ConflictException, ServerErrorException)
+from api.v2.schemas import MachineSchema
+from api.v2.services import MachineService
+from api.v2.schemas import ErrorSchema
 
 
 @api_controller("/machines")
 class MachineController(ControllerBase):
     
     @route("/")
-    def create(self):  
-        pass
+    def create(self, request : MachineSchema):  
+        try:
+            response = MachineService.create(request)
+            return 201, response
+        except ConflictException as e:
+            return 409, ErrorSchema(**e.__dict__)
+        except ServerErrorException as e:
+            return 500, ErrorSchema(**e.__dict__)
