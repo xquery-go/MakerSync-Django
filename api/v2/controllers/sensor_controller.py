3611ab9c35237b9111e671d9b2af2c20e3ff2191
xquery <best.highloyal@gmail.com>
2024-05-07 00:15:45 +0800

    Update api endpoint

--- api/v2/controllers/sensor_controller.py
+++ api/v2/controllers/sensor_controller.py
@@ -2,6 +2,6 @@ from ninja_extra import (
     api_controller, ControllerBase, route)
 
 
-@api_controller("/sensors")
+@api_controller("/machines/{code}/sensors")
 class SensorController(ControllerBase):
     pass
--- api/v2/controllers/user_controller.py
+++ api/v2/controllers/user_controller.py
@@ -2,6 +2,6 @@ from ninja_extra import (
     api_controller, ControllerBase, route)
 
 
-@api_controller("/users")
+@api_controller("/machines/{code}/users")
 class UserController(ControllerBase):
     pass
