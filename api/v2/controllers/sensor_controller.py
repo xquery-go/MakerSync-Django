678040411fcac39e6a91896f50d7975780f9350b
xquery <best.highloyal@gmail.com>
2024-05-07 00:22:47 +0800

    Add retrieve method in sensor_controller.py

--- api/v2/controllers/sensor_controller.py
+++ api/v2/controllers/sensor_controller.py
@@ -1,7 +1,13 @@
 from ninja_extra import (
     api_controller, ControllerBase, route)
+from api.v2.schemas import (
+    MachineSchema, SensorSchema)
 
 
 @api_controller("/machines/{code}/sensors")
 class SensorController(ControllerBase):
-    pass
+    
+    @route.get("/{sensor_id}")
+    def retrieve(self, code : MachineSchema, sensor_id : SensorSchema):
+        pass
+    
