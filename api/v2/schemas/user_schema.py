0e322c9bd9e4a940d594cc63509ec99314505169
xquery <best.highloyal@gmail.com>
2024-05-06 19:29:24 +0800

    Add user shema in user_schema.py

---
+++ api/v2/schemas/user_schema.py
@@ -0,0 +1,8 @@
+from pydantic import (
+    BaseModel, Field, EmailStr)
+
+
+class UserSchema(BaseModel):
+    
+    username : str = Field(..., title = "Username")
+    email : EmailStr = Field(..., title = "Email")
