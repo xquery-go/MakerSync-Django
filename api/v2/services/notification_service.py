643eaef6ece95ac91f8ade123a44a529be4a8eea
xquery <best.highloyal@gmail.com>
2024-05-07 10:31:16 +0800

    Add static retrieve method in notification_service.py

--- api/v2/services/notification_service.py
+++ api/v2/services/notification_service.py
@@ -3,3 +3,10 @@ class NotificationService:
     @staticmethod
     def list(code : MachineSchema):
         pass
+    
+    
+    @staticmethod
+    def retrieve(code : MachineSchema, notification_id : int):
+        pass
+    
+    
