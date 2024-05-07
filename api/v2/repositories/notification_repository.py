efe8db3eb81ded609e208ab5bea8863d48ed9abe
xquery <best.highloyal@gmail.com>
2024-05-07 16:39:21 +0800

    Update get_notification method implementation

--- api/v2/repositories/notification_repository.py
+++ api/v2/repositories/notification_repository.py
@@ -1,2 +1,17 @@
+from api.v2.schemas import (
+    MachineSchema, NotificationSchema)
+from api.v2.models import (
+    Notification, Machine)
+
+
 class NotificationRepository:
-    pass
+    
+    @staticmethod
+    def get_notifications(code : str):
+        machine = Machine.objects.get(code = code)
+        notifications = Notification.objects.filter(
+            machine = machine)
+
+        return [NotificationSchema(
+            **notification.__dict__).dict() 
+            for notification in notifications]
