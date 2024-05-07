a2d440d7133c0dbcc9efd26aa625cd293af97bc7
xquery <best.highloyal@gmail.com>
2024-05-07 16:38:11 +0800

    Update static list method in notification_service.py

--- api/v2/services/notification_service.py
+++ api/v2/services/notification_service.py
@@ -1,15 +1,27 @@
+from api.v2.repositories import (
+    MachineRepository, NotificationRepository)
+from api.v2.exceptions import (
+    NotFoundException, ServerErrorException)
+from api.v2.schemas import (
+    MachineSchema, NotificationSchema)
+
+
 class NotificationService:
     
     @staticmethod
-    def list(code : MachineSchema):
+    def list(machine_code : str):
+        
+        if not MachineRepository.is_machine_exist(machine_code):
+            raise NotFoundException()
+        
+        return NotificationRepository.get_notifications(machine_code)
+    
+    
+    @staticmethod
+    def retrieve(machine_id : MachineSchema, notification_id : int):
         pass
     
     
     @staticmethod
-    def retrieve(code : MachineSchema, notification_id : int):
-        pass
-    
-    
-    @staticmethod
-    def create(code : MachineSchema, request : NotificationSchema):
+    def create(machine_id : MachineSchema, request : NotificationSchema):
         pass
