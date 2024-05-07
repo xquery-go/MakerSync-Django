c8035575d30a0bc9dc1917c4eb7f411322067005
xquery <best.highloyal@gmail.com>
2024-05-07 18:23:38 +0800

    Add create notification shema export

--- api/v2/schemas/__init__.py
+++ api/v2/schemas/__init__.py
@@ -2,7 +2,8 @@ from api.v2.schemas.error_schema import ErrorSchema
 from api.v2.schemas.machine_schema import MachineSchema
 from api.v2.schemas.sensor_schema import SensorSchema
 from api.v2.schemas.user_schema import UserSchema
-from api.v2.schemas.notification_schema import NotificationSchema
+from api.v2.schemas.notification_schema import (
+    NotificationSchema, CreateNotificationSchema)
 
 
 __all__ = [
@@ -10,6 +11,7 @@ __all__ = [
     "MachineSchema", 
     "SensorSchema",
     "UserSchema",
-    "NotificationSchema"
+    "NotificationSchema",
+    "CreateNotificationSchema"
 ]
 
