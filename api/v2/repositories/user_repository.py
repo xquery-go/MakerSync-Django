989ff9ab6312b96fd84e36620a52f29c5557b679
xquery <best.highloyal@gmail.com>
2024-05-08 13:57:39 +0800

    Add repository get_users method implementation

--- api/v2/repositories/user_repository.py
+++ api/v2/repositories/user_repository.py
@@ -1,2 +1,14 @@
+from api.v2.models import (
+    Machine, User)
+
+
 class UserRepository:
-    pass
+    
+    @staticmethod 
+    def get_users(code : str):
+        
+        machine = Machine.objects.get(code = code)
+        users = User.objects.filter(
+            machine = machine).all()
+        
+        return users
