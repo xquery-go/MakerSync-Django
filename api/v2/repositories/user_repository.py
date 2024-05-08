4c58d1e8d12b70b1bd4e717e6c07fe3936cfa838
xquery <best.highloyal@gmail.com>
2024-05-08 14:08:59 +0800

    Add is_user_exist method in user_repository.py

--- api/v2/repositories/user_repository.py
+++ api/v2/repositories/user_repository.py
@@ -1,8 +1,24 @@
-from api.v2.models import (
+from pydantic import EmailStr
+from django.db.models import Q
+from api.v2.models import ( 
     Machine, User)
 
 
 class UserRepository:
+    
+    @staticmethod 
+    def is_user_exist(code : str, email : EmailStr):
+        
+        machine = Machine.objects.get(code = code)
+        user = User.objects.filter(
+            Q(machine = machine) & Q(email = email)
+        ).first()
+        
+        if user:
+            return True
+        
+        return False
+        
     
     @staticmethod 
     def get_users(code : str):
