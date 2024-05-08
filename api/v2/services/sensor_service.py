9fdd2ebc111ccdbd253e5d2fc93a0b017d7f5d25
xquery <best.highloyal@gmail.com>
2024-05-08 11:46:06 +0800

    Add service create method implementation

--- api/v2/services/sensor_service.py
+++ api/v2/services/sensor_service.py
@@ -22,3 +22,18 @@ class SensorService:
             raise ServerErrorException()
     
         return SensorSchema(**sensor.__dict__).dict()
+    
+    
+    @staticmethod
+    def create(machine_code : str):
+        
+        if not MachineRepository.is_machine_exist(machine_code):
+            raise NotFoundException()
+        
+        sensor = SensorRepository.create_sensor(
+            machine_code)
+        
+        if not sensor:
+            raise ServerErrorException()
+        
+        return SensorSchema().dict()
