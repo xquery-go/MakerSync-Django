490d24a98410eaecc92f7346cdadc7b85f1ea9b2
xquery <best.highloyal@gmail.com>
2024-05-08 13:57:59 +0800

    Add service list method implementation

--- api/v2/services/user_service.py
+++ api/v2/services/user_service.py
@@ -1,2 +1,27 @@
+from api.v2.repositories import (
+    MachineRepository, UserRepository)
+from api.v2.exceptions import  (
+    NotFoundException, ServerErrorException, 
+    BadRequestException)
+from api.v2.schemas import (
+    UserSchema)
+
 class UserService:
-    pass
+    
+    @staticmethod
+    def list(machine_code : str):
+        
+        if not MachineRepository.is_machine_exist(
+            machine_code):
+            raise NotFoundException()
+        
+        users = UserRepository.get_users(machine_code)
+        
+        if not users:
+            return []
+        
+        response = [UserSchema(**user.__dict__).dict() 
+                    for user in users]
+        
+        return response
+        
