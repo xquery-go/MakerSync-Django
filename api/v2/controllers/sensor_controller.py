9c616f82a831136f64da152c12f3992ad5ae9520
xquery <best.highloyal@gmail.com>
2024-05-08 11:39:10 +0800

    Update controller retrieve method implementation

--- api/v2/controllers/sensor_controller.py
+++ api/v2/controllers/sensor_controller.py
@@ -2,15 +2,27 @@ from ninja_extra import (
     api_controller, ControllerBase, route)
 from api.v2.schemas import (
     MachineSchema, SensorSchema)
+from api.v2.services import (
+    SensorService)
+from api.v2.exceptions import (
+    ConflictException, ServerErrorException,
+    NotFoundException)
+from api.v2.schemas import ErrorSchema
 
 
 @api_controller("/machines/{machine_code}/sensors")
 class SensorController(ControllerBase):
     
-    @route.get("/{sensor_id}")
-    def retrieve(self, machine_code : str, sensor_id : int):
-        pass
-    
+    @route.get("/")
+    def retrieve(self, machine_code : str):
+        try:
+            response = SensorService.retrieve(machine_code)
+            return response
+        except NotFoundException as e:
+            return ErrorSchema(**e.__dict__)
+        except ServerErrorException as e:
+            return ErrorSchema(**e.__dict__)
+            
     
     @route.post("/")
     def create(self, machine_code : str, request : SensorSchema):
