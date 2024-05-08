323b11bbbdefe9123bc5baa945a03ec79b8137a4
xquery <best.highloyal@gmail.com>
2024-05-08 11:38:56 +0800

    Add service retrieve method implementation

--- api/v2/services/sensor_service.py
+++ api/v2/services/sensor_service.py
@@ -1,2 +1,24 @@
+from api.v2.repositories import (
+    SensorRepository, MachineRepository)
+from api.v2.exceptions import (
+    ConflictException, NotFoundException,
+    ServerErrorException)
+from api.v2.schemas import (
+    SensorSchema)
+
+
 class SensorService:
-    pass
+    
+    @staticmethod
+    def retrieve(machine_code : str):
+        
+        if not MachineRepository.is_machine_exist(machine_code):
+            raise NotFoundException()
+        
+        sensor = SensorRepository.get_sensor(
+            machine_code)
+        
+        if not sensor:
+            raise ServerErrorException()
+    
+        return SensorSchema(**sensor.__dict__).dict()
