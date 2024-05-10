e762dc162ef42b0681e5e07bc7f25a4fcefd713a
xquery <best.highloyal@gmail.com>
2024-05-10 23:12:20 +0800

    Add docs in sensor retrieve method

--- api/v2/controllers/sensor_controller.py
+++ api/v2/controllers/sensor_controller.py
@@ -12,16 +12,35 @@ from api.v2.schemas import ErrorSchema
 
 @api_controller("/machines/{machine_code}/sensors")
 class SensorController(ControllerBase):
+    """
+    Controller handling the Sensor instance.
+    """
     
-    @route.get("/")
+    @route.get("/",
+               summary = "Retrieves a sensor instance.",
+               description = "Retrieves a sensor instance based on the path parameter inputted",
+               response = {
+                   200 : SensorSchema,
+                   404 : ErrorSchema,
+                   500 : ErrorSchema
+               })
     def retrieve(self, machine_code : str):
+        """
+        Endpoint to retrieves a sensor instance based on the machine code.
+
+        Args:
+            machine_code (str): The ID of the machine.
+
+        Returns:
+            tuple: A tuple containing status code and response data.
+        """
         try:
             response = SensorService.retrieve(machine_code)
-            return response
+            return 200, response
         except NotFoundException as e:
-            return ErrorSchema(**e.__dict__)
+            return 404, ErrorSchema(**e.__dict__)
         except ServerErrorException as e:
-            return ErrorSchema(**e.__dict__)
+            return 500, ErrorSchema(**e.__dict__)
             
     
     @route.post("/")
