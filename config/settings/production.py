98304e585f701c92e8b48f0d66369182662e72c9
xquery <best.highloyal@gmail.com>
2024-05-11 00:15:44 +0800

    Add postgresql database constant

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from config.settings.base import *
 
 
@@ -6,5 +8,15 @@ DEBUG = False
 ALLOWED_HOSTS = [
     ".vercel.app", 
     ".now.sh",
-    'maker-sync-django.vercel.app']
+    "maker-sync-django.vercel.app"]
 
+DATABASES = { 
+	"default": { 
+		"ENGINE": "django.db.backends.postgresql_psycopg2", 
+		"HOST": os.environ.env("HOST"), 
+		"NAME": os.environ.env("NAME"), 
+		"USER": os.environ.env("USER"), 
+		"PASSWORD": os.environ.env("PASSWORD"), 
+		"PORT": os.environ.env("PORT"), 
+	} 
+}
