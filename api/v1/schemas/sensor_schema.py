86d0894a455ef3ce5cc83403ffcacefd9ae557bb
xquery <best.highloyal@gmail.com>
2024-05-10 13:48:49 +0800

    Update sensor schema fields

--- api/v1/schemas/sensor_schema.py
+++ api/v1/schemas/sensor_schema.py
@@ -8,14 +8,14 @@ class CreateSensorSchema(BaseModel):
 
 class SensorSchema(BaseModel):
     is_start : bool = Field(
-        ..., title = "Sensor Is Start")
+        title = "Sensor Is Start", default = False)
     is_stop : bool = Field(
-        ..., title = "Sensor Is Stop")
+        title = "Sensor Is Stop", default = False)
     is_initialize : bool = Field(
-        ..., title = "Sensor Is Initialize")
+        title = "Sensor Is Initialize", default = False)
     counter : int = Field(
-        ..., title = "Sensor Counter")
+        title = "Sensor Counter", default = 0)
     time : int = Field(
-        ..., title = "Sensor Time")
-    temperature : float=Field(
-        ..., title = "Sensor Temperature")
+        title = "Sensor Time", default = 0)
+    temperature : float = Field(
+        title = "Sensor Temperature", default = 0.0)
